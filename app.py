1512e4308d334ab94b656efa42aa5b126a584a7f
Aryan <theredsword70@gmail.com>
2023-09-22 22:56:17 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template ,request,redirect
+
+app = Flask(__name__)
+@app.route('/')
+def man():
+    return render_template('a.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route("/calculate",methods=['POST'])
+def calculate_exp():
+    try:
+        if request.method=='POST':
+            exp=request.form['expression']
+            if exp=="" or exp.isalpha():
+                return render_template('a.html',exp="Invalid or No Input")
+            print(exp)
+            return render_template('a.html',exp=eval(exp))
+    except:
+        return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
